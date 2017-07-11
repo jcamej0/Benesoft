f97d8526851a9ac90bcd100bda544e8e9f8c85ec
Juan <jcamejob@gmail.com>
2017-07-11 16:38:02 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(cookieParser());
 app.use(session({ secret: 'secreto' }));
 app.use('/', routes);
 app.use('/users', users);
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() { 
 
     console.log("HOLA COMO ESTAS")
